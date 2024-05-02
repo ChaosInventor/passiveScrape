d7308dbb7f189d77fa47d58c7d10cda466790ad5
ChaosInventor <chaosinventor@yandex.com>
2024-05-02 20:48:30 +0200

    Add new tables

--- server/create-tables.sql
+++ server/create-tables.sql
@@ -1,5 +1,16 @@
-CREATE TABLE dom (
+CREATE TABLE IF NOT EXISTS dom (
     url     text        NOT NULL,
     data    bytea       ,
     time    timestamp   PRIMARY KEY DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tracked (
+    url     text        PRIMARY KEY,
+    added   timestamp   UNIQUE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS trackingInterval (
+    url     text        REFERENCES tracked(url),
+    since   timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    until   timestamp
 );
